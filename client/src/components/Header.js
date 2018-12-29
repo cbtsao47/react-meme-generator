68fab2ed044ced9b2333470751d68193b9962462
wisdomtooth <cbtsao47@gmail.com>
2018-12-28 20:37:02 -0600

    add header css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,12 +2,13 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className="header">
       <img
         src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
         alt="troll face"
+        className="header__logo"
       />
-      <h1>Meme of the day</h1>
+      <h1 className="header__title">Meme of the day</h1>
     </header>
   );
 }
