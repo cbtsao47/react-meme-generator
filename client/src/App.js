3e083a6f0e7557cd09b5aa7f2487ee38ee7a2771
wisdomtooth <cbtsao47@gmail.com>
2018-12-28 18:07:06 -0600

    initialize meme generator

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Header />
-        <MainContent />
-        <Footer />
-      </div>
-    );
+    return <div className="App">something</div>;
   }
 }
 
--- client/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Footer() {
-  return <footer className="footer">footer</footer>;
-}
-
-export default Footer;
